bfa3f10b1b19e853bca7ea5e403ee820af5696cd
Joel Jose Mathew <joeljm97@gmail.com>
2020-10-09 01:12:27 +0530

    fixed write to pass.csv

--- testAPP2.cpp
+++ testAPP2.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 typedef vector<string> stringvec;
-string path = "D:\\file.html";
+string path = "C:\\Results.html";
 class APP2
 {
 private:
@@ -17,16 +17,24 @@ private:
 
     string removeTag(string str)
     {
-        for (int i = 0;i < str.length();i++)
-        {
-            int beg,end;
-            string s1;
-            beg = indexOf(str, "<td>");
-            beg = beg + 4;
-            end = indexOf(str, "</td>");
-            s1 = string(&str[beg], &str[end]);
-            return (s1);
-        }
+        int beg,end;
+        string s1;
+        beg = indexOf(str, "<td>");
+        beg = beg + 4;
+        end = indexOf(str, "</td>");
+        end = end;
+        s1 = string(&str[beg], &str[end]);
+        return (s1);
+
+    }
+    string cleanLogLoc(string str)
+    {
+        int beg, end;
+        string s1;
+        beg = indexOf(str, "file:/");
+        end = indexOf(str, "><code>")-1;
+        s1 = string(&str[beg], &str[end]);
+        return (s1);
 
     }
     int indexOf(string s, string s1) //get the index position of a character or substring in a String
@@ -57,9 +65,9 @@ private:
         pass.open("pass.csv");
         fail.open("fail.csv");
         abort.open("abort.csv");
-        pass << string("Date_Time,Log_Location,Parameters\n");
-        fail << string("Date_Time,Log_Location,Parameters,Error_Description\n");
-        abort << string("Date_Time,Log_Location,Parameters,Error_Description\n");
+        pass << string("Date_Time,Test_Name,Parameters,Log_Location\n");
+        fail << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n");
+        abort << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n");
         pass.close();
         fail.close();
         abort.close();
@@ -69,7 +77,8 @@ private:
     {
         
         string fileLoc(path),line,date="";
-        
+        initializeFiles();
+        pass.open("pass.csv",ios::app);
         //getline(cin, fileLoc);
         fs.open(fileLoc);
         skip(fs,3);
@@ -87,11 +96,26 @@ private:
                 if (line.find("<tr>")>linelen && line.find("</tr>") > linelen)
                 {
                     line=removeTag(line);
-                    if (line == "Pass")
+                    if (line == "PASSED")
                     {
-
+                        pass << date ;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            getline(fs, line);
+                            if (i!=2) 
+                            {
+                                line = "," + removeTag(line) ;
+                            }
+                            else
+                            {
+                                line = removeTag(line);
+                                line = "," +cleanLogLoc(line) ;
+                            }
+                            pass << line;
+                        }
+                        pass << "\n";
                     }
-                    pass<< line << endl;
+                    
                 }
                 date = line;
             }
