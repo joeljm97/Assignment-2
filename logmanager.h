98f9dd2a7fcbdce0d3c6493e702fc06752872351
Joel Mathew <joeljm97@gmail.com>
2020-10-12 17:16:03 +0530

    Added logging functionality

---
+++ logmanager.h
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+string getTimeStr() // returns the timestamp
+{
+    time_t now = time(0);
+    string dt = ctime(&now);
+    string st = "";
+    for (int i = 0;i < dt.length() - 1;i++)
+    {
+        st = st + dt[i];
+    }
+    return st;
+}
+
+string createlogname() //creates a formatted valid name for log file using time stamp
+{
+    int count = 0;
+    string str = "log_" + getTimeStr(), str1 = "";
+    for (int i = 0;i < str.length();i++)
+    {
+        if (str[i] == ' ')
+            str1 = str1 + '_';
+        else if (str[i] == ':')
+            str1 = str1 + '-';
+        else
+            str1 = str1 + str[i];
+    }
+    str1 = str1 + ".log";
+    return(str1);
+}
+string str = createlogname();
+void initializeLog() // creating the log file and adding the column names
+{
+    
+    ofstream log;
+    log.open(str);
+    log << "Start Time: " << getTimeStr() << "\n";
+    if (!log)
+        cout << "creation failed";
+    else
+    {
+        log << left << setw(20) << setfill(' ') << "Serial number";
+        log << left << setw(30) << setfill(' ') << "Timestamp";
+        log << left << setw(20) << setfill(' ') << "Line number";
+        log << left << setw(50) << setfill(' ') << "Event";
+        log << "\n";
+    }
+    log.close();
+}
+
+string funcName(const char* func) //returns current function name as a string 
+{
+    string s;
+    s.assign(func);
+    s = s + "()";
+    return s;
+}
+
+void writetolog(int line, string msg)
+{
+    int static serial = 1;
+    ofstream log;
+    log.open(str, ios::app);
+    log << left << setw(20) << setfill(' ') << serial++;
+    log << left << setw(30) << setfill(' ') << getTimeStr();
+    log << left << setw(20) << setfill(' ') << line;
+    log << left << setw(50) << setfill(' ') << msg;
+    log << "\n";
+}
+
+void endlog()
+{
+    ofstream log;
+    log.open(str, ios::app);
+    log << "\nApplication ran successfully with no error\n";
+    log << "\nEnd Time: " << getTimeStr()<<endl;
+    log.close();
+}
--- testAPP2.cpp
+++ testAPP2.cpp
@@ -1,8 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
-#include<iterator>
+#include <iterator>
+#include "logmanager.h"
 
 using namespace std;
 
@@ -16,41 +18,35 @@ private:
     
     ofstream pass, fail, abort;
 
-    int indexOf(string s, string s1) //get the index position of a character or substring in a String
+    string removeTag(string str) //gets string in between a <td></td> tag
     {
-        string::size_type loc = s.find(s1);
-        if ((int)loc < s.length())
-        {
-            return int(loc);
-        }
-        else
-        {
-            return -1;
-        }
-    }
-
-    string removeTag(string str)
-    {
-        int beg, end;
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+        string::size_type beg, end;
         string s1;
-        beg = indexOf(str, "<td>");
+        beg = str.find("<td>");
         beg = beg + 4;
-        end = indexOf(str, "</td>");
+        end = str.find("</td>");
         end = end;
         s1 = string(&str[beg], &str[end]);
+        writetolog(__LINE__, "Returned value : "+s1+" From " + funcName(__FUNCTION__));
         return (s1);
     }
-    string formatLogLoc(string str)
+
+    string formatLogLoc(string str) //Formats log location string by removing additional <code> tags
     {
-        int beg, end;
-        string s1, s2 = "";
-        beg = indexOf(str, "file:/");
-        end = indexOf(str, "><code>") - 1;
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+        string::size_type beg, end;
+        string s1;
+        beg = str.find("file:/");
+        end = str.find("><code>") - 1;
         s1 = string(&str[beg], &str[end]);
+        writetolog(__LINE__, "Returned value : " + s1 + " From " + funcName(__FUNCTION__));
         return(s1);
     }
-    string formatError(string str)
+
+    string formatError(string str) //Formats error code so that it can be added in a csv file without issues 
     {
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         string s1;
         for (int i = 0; i < str.length(); i++)
         {
@@ -60,10 +56,13 @@ private:
                 s1 = s1 + "\"\"";
         }
         s1 = "\"" + s1 + "\"";
+        writetolog(__LINE__, "Returned value : " + s1 + " From "  + funcName(__FUNCTION__));
         return (s1);
     }
-    void skip(ifstream & f, int n)
+
+    void skip(ifstream& f, int n) //function to skip n number of lines in a file 
     {
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         string line;
         int count = 0;
         while (getline(f, line))
@@ -72,58 +71,77 @@ private:
             if (count == n + 1)
                 break;
         }
+        writetolog(__LINE__, "Exited: " + funcName(__FUNCTION__));
     }
 
-    public:void initializeFiles()
+    void initializeFiles() // function to initialize the three csv files with column names
     {
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         pass.open("pass.csv");
         fail.open("fail.csv");
         abort.open("abort.csv");
-        pass << string("Date_Time,Test_Name,Parameters,Log_Location\n");
-        fail << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n");
-        abort << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n");
+        pass << string("Date_Time,Test_Name,Parameters,Log_Location\n"); writetolog(__LINE__, "pass.csv intialized");
+        fail << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); writetolog(__LINE__, "fail.csv intialized");
+        abort << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); writetolog(__LINE__, "abort.csv intialized");
         pass.close();
         fail.close();
         abort.close();
+        writetolog(__LINE__, "Exited: " + funcName(__FUNCTION__));
     }
 
-    void map_to_file(stringmap mp, ofstream& fo)
+    void map_to_file(stringmap mp, ofstream& fo) //function to convert a STL::MAP to file
     {
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         stringmap::reverse_iterator itr;
         for (itr = mp.rbegin(); itr != mp.rend(); ++itr)
         {
             fo << itr->first<< itr->second << '\n';
         }
+        writetolog(__LINE__, "Exited : " + funcName(__FUNCTION__));
     }
 
-    void mainFunc()
+    public:void mainFunc()  
     {
-
+        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         string fileLoc, line, date = "";
-        initializeFiles();
-        pass.open("pass.csv", ios::app);
-        fail.open("fail.csv", ios::app);
-        abort.open("abort.csv", ios::app);
+        cout << "Enter file Location: ";
+        writetolog(__LINE__, "waiting for user input");
         getline(cin, fileLoc);
-        fs.open(fileLoc);
+        writetolog(__LINE__, "user entered :"+fileLoc);
+        fs.open(fileLoc); //open results.html file
         skip(fs, 3);
-        if (!fs)
+        while (!fs)
         {
-            cout << "Failed to open file";
+            cout << "\n\nUnable to open file\nTry again!!!!!!!\n\n";
+            cout << "Enter file Location: ";
+            getline(cin, fileLoc);
+            fs.open(fileLoc);
         }
-        else
+        if (!fs) //if file opening fails
         {
-            while (getline(fs, line))
+            cout << "Failed to open file\n";
+        }
+
+        else //if file opening is successful
+        {
+
+            initializeFiles();
+            pass.open("pass.csv", ios::app);
+            fail.open("fail.csv", ios::app);
+            abort.open("abort.csv", ios::app);
+
+            while (getline(fs, line)) //get each line from Results.html
             {
                 
                 int linelen = line.length();
-                if (line.find("</table>") < linelen)
+                if (line.find("</table>") < linelen) // break while loop when it reaches </table> i.e, end of data
                     break;
                 if (line.find("<tr>") > linelen && line.find("</tr>") > linelen)
                 {
                     line = removeTag(line);
                     if (line == "PASSED")
                     {
+                        writetolog(__LINE__, "Passed case met");
                         string str = "";
                         for (int i = 0; i < 3; i++)
                         {
@@ -139,10 +157,12 @@ private:
                             }
                             str = str + line;
                         }
+                        writetolog(__LINE__, "inserting to map: m_pass");
                         m_pass.insert({ date, str });
                     }
                     else if (line == "FAILED")
                     {
+                        writetolog(__LINE__, "Failed case met");
                         string str = "";
                         for (int i = 0; i < 4; i++)
                         {
@@ -158,11 +178,12 @@ private:
                             }
                             str = str +","+ line;
                         }
-                        cout << str<<endl;
+                        writetolog(__LINE__, "inserting to map: m_fail");
                         m_fail.insert({ date, str });
                     }
                     else if (line == "ABORTED")
                     {
+                        writetolog(__LINE__, "Aborted case met");
                         string str = "";
                         for (int i = 0; i < 4; i++)
                         {
@@ -178,12 +199,14 @@ private:
                             }
                             str = str+ "," + line;
                         }
+                        writetolog(__LINE__, "inserting to map: m_abort");
                         m_abort.insert({ date, str });
                     }
 
                 }
-                date = line;
+                date = line; 
             }
+            cout << "\nData has been separated.\n\n";
         }
         map_to_file(m_pass, pass);
         pass.close();
@@ -191,12 +214,17 @@ private:
         fail.close();
         map_to_file(m_abort, abort);
         abort.close();
+        
+        writetolog(__LINE__, "Exited : " + funcName(__FUNCTION__));
     }
 };
 
-    int main()
-    {
-        APP2 A1;
-        A1.mainFunc();
-
-    }
+int main()
+{
+    initializeLog();
+    APP2 A1; 
+    writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+    A1.mainFunc(); 
+    endlog();
+    system("pause");
+}
