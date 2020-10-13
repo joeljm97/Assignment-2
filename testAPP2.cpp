8d42a589f99f6e997d9724f7d9d7a01ff8d83d8f
Joel Jose Mathew <joeljm97@gmail.com>
2020-10-13 13:34:01 +0530

    cleaned log file , removed unwanted functions

--- testAPP2.cpp
+++ testAPP2.cpp
@@ -20,7 +20,7 @@ private:
 
     string removeTag(string line) //gets string in between a <td></td> tag
     {
-        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+        
         string::size_type beg, end;
         string temp_line;
         beg = line.find("<td>");
@@ -28,25 +28,25 @@ private:
         end = line.find("</td>");
         end = end;
         temp_line = string(&line[beg], &line[end]);
-        writetolog(__LINE__, "Returned value : "+ temp_line +" From " + funcName(__FUNCTION__));
+        
         return (temp_line);
     }
 
     string formatLogLoc(string line) //Formats log location string by removing additional <code> tags
     {
-        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+       
         string::size_type beg, end;
         string temp_line;
         beg = line.find("file:/");
         end = line.find("><code>") - 1;
         temp_line = string(&line[beg], &line[end]);
-        writetolog(__LINE__, "Returned value : " + temp_line + " From " + funcName(__FUNCTION__));
+        
         return(temp_line);
     }
 
     string formatError(string line) //Formats error code so that it can be added in a csv file without issues 
     {
-        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+        
         string temp_line;
         for (int i = 0; i < line.length(); i++)
         {
@@ -56,22 +56,8 @@ private:
                 temp_line = temp_line + "\"\"";
         }
         temp_line = "\"" + temp_line + "\"";
-        writetolog(__LINE__, "Returned value : " + temp_line + " From "  + funcName(__FUNCTION__));
+        
         return (temp_line);
-    }
-
-    void skip(ifstream& f, int n) //function to skip n number of lines in a file 
-    {
-        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
-        string line;
-        int count = 0;
-        while (getline(f, line))
-        {
-            count++;
-            if (count == n + 1)
-                break;
-        }
-        writetolog(__LINE__, "Exited: " + funcName(__FUNCTION__));
     }
 
     void initializeFiles() // function to initialize the three csv files with column names
@@ -80,9 +66,12 @@ private:
         pass.open("pass.csv");
         fail.open("fail.csv");
         abort.open("abort.csv");
-        pass << string("Date_Time,Test_Name,Parameters,Log_Location\n"); writetolog(__LINE__, "pass.csv intialized");
-        fail << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); writetolog(__LINE__, "fail.csv intialized");
-        abort << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); writetolog(__LINE__, "abort.csv intialized");
+        pass << string("Date_Time,Test_Name,Parameters,Log_Location\n"); 
+        writetolog(__LINE__, "pass.csv intialized");
+        fail << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); 
+        writetolog(__LINE__, "fail.csv intialized");
+        abort << string("Date_Time,Test_Name,Parameters,Log_Location,Error_Description\n"); 
+        writetolog(__LINE__, "abort.csv intialized");
         pass.close();
         fail.close();
         abort.close();
@@ -91,30 +80,33 @@ private:
 
     void map_to_file(stringmap mp, ofstream& fo) //function to convert a STL::MAP to file
     {
-        writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
+        writetolog(__LINE__, "Copying map contents to File");
         stringmap::reverse_iterator itr;
         for (itr = mp.rbegin(); itr != mp.rend(); ++itr)
         {
             fo << itr->first<< itr->second << '\n';
         }
-        writetolog(__LINE__, "Exited : " + funcName(__FUNCTION__));
     }
 
     public:void mainFunc()  
     {
         writetolog(__LINE__, "Entered: " + funcName(__FUNCTION__));
         string fileLoc, line, date = "";
-        cout << "Enter file Location: ";
+        cout << "Enter file directory: ";
         writetolog(__LINE__, "waiting for user input");
         getline(cin, fileLoc);
-        writetolog(__LINE__, "user entered :"+fileLoc);
+        writetolog(__LINE__, "user entered: " + fileLoc);
+        fileLoc = fileLoc + "\\Results.html";
         inputFile.open(fileLoc); //open results.html file
-        skip(inputFile, 3);
+
         while (!inputFile)
         {
             cout << "\n\nUnable to open file\nTry again!!!!!!!\n\n";
+            writetolog(__LINE__, "Unable to open file, waiting for user input" );
             cout << "Enter file Location: ";
             getline(cin, fileLoc);
+            writetolog(__LINE__, "user entered: " + fileLoc);
+            fileLoc = fileLoc + "\\Results.html";
             inputFile.open(fileLoc);
         }
         if (!inputFile) //if file opening fails
@@ -124,7 +116,7 @@ private:
 
         else //if file opening is successful
         {
-
+            writetolog(__LINE__, "Results.html opened successfully");
             initializeFiles();
             pass.open("pass.csv", ios::app);
             fail.open("fail.csv", ios::app);
@@ -136,12 +128,12 @@ private:
                 int linelen = line.length();
                 if (line.find("</table>") < linelen) // break while loop when it reaches </table> i.e, end of data
                     break;
-                if (line.find("<tr>") > linelen && line.find("</tr>") > linelen)
+                if (line.find("<tr>") > linelen && line.find("</tr>") > linelen && line.find("<td>") < linelen)
                 {
                     line = removeTag(line);
                     if (line == "PASSED")
                     {
-                        writetolog(__LINE__, "Passed case met");
+                        
                         string str = "";
                         for (int i = 0; i < 3; i++)
                         {
@@ -157,12 +149,12 @@ private:
                             }
                             str = str + line;
                         }
-                        writetolog(__LINE__, "inserting to map: m_pass");
+                        
                         m_pass.insert({ date, str });
                     }
                     else if (line == "FAILED")
                     {
-                        writetolog(__LINE__, "Failed case met");
+                        
                         string str = "";
                         for (int i = 0; i < 4; i++)
                         {
@@ -178,12 +170,12 @@ private:
                             }
                             str = str +","+ line;
                         }
-                        writetolog(__LINE__, "inserting to map: m_fail");
+                        
                         m_fail.insert({ date, str });
                     }
                     else if (line == "ABORTED")
                     {
-                        writetolog(__LINE__, "Aborted case met");
+                        
                         string str = "";
                         for (int i = 0; i < 4; i++)
                         {
@@ -199,7 +191,7 @@ private:
                             }
                             str = str+ "," + line;
                         }
-                        writetolog(__LINE__, "inserting to map: m_abort");
+                        
                         m_abort.insert({ date, str });
                     }
 
@@ -208,11 +200,17 @@ private:
             }
             cout << "\nData has been separated.\n\n";
         }
+        writetolog(__LINE__, "Writing to pass.csv");
         map_to_file(m_pass, pass);
         pass.close();
+        writetolog(__LINE__, "Write to pass.csv complete");
+        writetolog(__LINE__, "Writing to fail.csv");
         map_to_file(m_fail, fail);
         fail.close();
+        writetolog(__LINE__, "Write to fail.csv complete");
+        writetolog(__LINE__, "Writing to abort.csv");
         map_to_file(m_abort, abort);
+        writetolog(__LINE__, "Write to abort.csv complete");
         abort.close();
         
         writetolog(__LINE__, "Exited : " + funcName(__FUNCTION__));
